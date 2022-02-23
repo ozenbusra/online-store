99eb7107c1b3d532f3af16e66240bd331a37fb21
Busra Ozen <ozenbusrab@gmail.com>
2022-02-23 23:29:43 +0300

    add items controller and views
    Signed-off-by: Busra Ozen <ozenbusrab@gmail.com>

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,54 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    @store_items = StoreItem.filter_by_item_id(params[:id])
+    @stores = []
+    @store_items.each do |store_item|
+      @stores << Store.find(store_item.store_id)
+    end
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    
+    if @item.save
+      redirect_to @item
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+
+    redirect_to items_path, status: :see_other 
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:item_name)
+    end
+end
