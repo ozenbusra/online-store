dd112cc32d8649b9554f8c30beb556bd80da7a44
Busra Ozen <ozenbusrab@gmail.com>
2022-02-23 23:31:43 +0300

    add stores controller and views
    Signed-off-by: Busra Ozen <ozenbusrab@gmail.com>

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,54 @@
+class StoresController < ApplicationController
+  def index
+    @stores = Store.all
+  end
+
+  def show
+    @store = Store.find(params[:id])
+    @store_items = StoreItem.filter_by_store_id(params[:id])
+    @items = []
+    @store_items.each do |store_item|
+      @items << Item.find(store_item.item_id)
+     end  
+  end
+  
+  def new
+    @store = Store.new
+  end
+
+  def create
+    @store = Store.new(store_params)
+    
+    if @store.save
+      redirect_to @store
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  def edit
+    @store = Store.find(params[:id])
+  end
+
+  def update
+    @store = Store.find(params[:id])
+
+    if @store.update(store_params)
+      redirect_to @store
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @store = Store.find(params[:id])
+    @store.destroy
+
+    redirect_to stores_path, status: :see_other 
+  end
+
+  private
+    def store_params
+      params.require(:store).permit(:store_name, :location_id)
+    end
+end
