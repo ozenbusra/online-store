5734c0acd4afc16942299acc832b8d8f9369068d
Busra Ozen <ozenbusrab@gmail.com>
2022-02-28 23:06:47 +0300

    handle RecordNotUnique exception
    Signed-off-by: Busra Ozen <ozenbusrab@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,17 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
 
   private
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to root_path
+  end
+
+  def record_not_unique
+    flash[:alert] = "It already exists."
+    request.referrer ? (redirect_to request.referrer) : (redirect_to root_path)
   end
 end
