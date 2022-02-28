27d4acbd9e46409a5dee127c6475787a46591ace
Busra Ozen <ozenbusrab@gmail.com>
2022-02-28 23:06:14 +0300

    change user's default role id
    Signed-off-by: Busra Ozen <ozenbusrab@gmail.com>

--- db/migrate/20220225113301_devise_create_users.rb
+++ db/migrate/20220225113301_devise_create_users.rb
@@ -32,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.references :role, null: false, default: 1, foreign_key: { on_delete: :cascade }
+      t.references :role, null: false, default: 2, foreign_key: { on_delete: :cascade }
       t.timestamps null: false
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_26_162503) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "role_id", default: 1, null: false
+    t.integer "role_id", default: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
